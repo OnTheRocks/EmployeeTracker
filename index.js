de9f974eb241368ab2e7fb535c04e0d3823c8029
Nathan Huber <Nathan_Huber@yahoo.com>
2020-01-23 03:11:07 -0600

    Got the 1st few funtions/queries working, started a switch/case to work through the user prompts.

--- index.js
+++ index.js
@@ -4,165 +4,120 @@ var inquirer = require("inquirer");
 // create the connection information for the sql database
 var connection = mysql.createConnection({
   host: "localhost",
-
-  // Your port; if not 3306
   port: 3306,
-
-  // Your username
   user: "OnTheRocks",
-
-  // Your password
   password: "OnTheRocks",
   database: "tracker_DB"
 });
+
 
 // connect to the mysql server and sql database
 connection.connect(function(err) {
   if (err) throw err;
 
   console.log("You are connected to the Tracker Database.")
+
+  console.log("Welcome, to this amazing app that will allow you to track lots and lots of Employees.")
   // run the start function after the connection is made to prompt the user
   start();
 });
 
 // function which prompts the user for what action they should take
 function start() {
-  console.log("Welcome, to this amazing app that will allow you to track lots and lots of Employees.")
+  
   inquirer
     .prompt({
-      name: "test1",
+      name: "main",
       type: "list",
-      message: "Would you like to list employees Y/N?",
-      choices: ["Yes", "No"]
+      message: "What would you like to do?",
+      choices: [
+                "View All Employees", 
+                "View All Employees By Department",
+                "View All Employees By Manager",
+                "Add Employee",
+                "Remove Employee",
+                "Update Employee Role",
+                "Exit"
+              ]
     })
     .then(function(answer) {
       // based on their answer, 
-      if (answer.test1 === "Yes") {
-        listEmployees();
- 
-      } else{
-        connection.end();
+      
+      switch (answer.main) {
+        case "View All Employees":
+          listEmployees();
+          break;
+
+        case "View All Employees By Department":
+          listByDept();
+          break;
+
+        case "View All Employees By Manager":
+          listByMngr();
+          break;
+
+        case "Exit":          
+         
+            console.log("Have a GREAT day!");
+            connection.end();
+            process.exit();
       }
-    });
-}
+
 
 // function to handle listing employees
 function listEmployees() {
 
-  connection.query("SELECT * FROM employee", function(err, results) {
-        if (err) throw err;
+  connection.query("Select e.id, first_name, last_name, title, d.name, salary from employee e, role r, department d where e.role_id = r.id and r.department_id = d.id", function(err, results) {
+    if (err) throw err;
+    console.log();
+    console.log();
+    console.log();
+    console.log();
+    console.log();
+    console.table(results);
+    start();    
+  });
+}
 
-        console.log(results);
-    connection.end();
+function listByDept() {
+
+  connection.query("Select e.id, first_name, last_name, title, d.name, salary from employee e, role r, department d where e.role_id = r.id and r.department_id = d.id order by d.name", function(err, results) {
+    if (err) throw err;
+    console.log();
+    console.log();
+    console.log();
+    console.log();
+    console.log();
+    console.table(results);
+    start();
+  });
+}
+
+function listByMngr() {
+
+  connection.query("Select e.id, first_name, last_name, title, d.name, salary, manager_id from employee e, role r, department d where e.role_id = r.id and r.department_id = d.id order by manager_id", function(err, results) {
+    if (err) throw err;
+    console.log();
+    console.log();
+    console.log();
+    console.log();
+    console.log();
+    console.table(results);
+    start();
+  });
+}
 
 
-      }
-      )};
 
-  // prompt for info about the item being put up for auction
-//   inquirer
-//     .prompt([
-//       {
-//         name: "item",
-//         type: "input",
-//         message: "What is the item you would like to submit?"
-//       },
-//       {
-//         name: "category",
-//         type: "input",
-//         message: "What category would you like to place your auction in?"
-//       },
-//       {
-//         name: "startingBid",
-//         type: "input",
-//         message: "What would you like your starting bid to be?",
-//         validate: function(value) {
-//           if (isNaN(value) === false) {
-//             return true;
-//           }
-//           return false;
-//         }
-//       }
-//     ])
-//     .then(function(answer) {
-//       // when finished prompting, insert a new item into the db with that info
-//       connection.query(
-//         "INSERT INTO auctions SET ?",
-//         {
-//           item_name: answer.item,
-//           category: answer.category,
-//           starting_bid: answer.startingBid || 0,
-//           highest_bid: answer.startingBid || 0
-//         },
-//         function(err) {
-//           if (err) throw err;
-//           console.log("Your auction was created successfully!");
-//           // re-prompt the user for if they want to bid or post
-//           start();
-//         }
-//       );
-//     });
-// }
 
-// function bidAuction() {
-//   // query the database for all items being auctioned
-//   connection.query("SELECT * FROM auctions", function(err, results) {
-//     if (err) throw err;
-//     // once you have the items, prompt the user for which they'd like to bid on
-//     inquirer
-//       .prompt([
-//         {
-//           name: "choice",
-//           type: "rawlist",
-//           choices: function() {
-//             var choiceArray = [];
-//             for (var i = 0; i < results.length; i++) {
-//               choiceArray.push(results[i].item_name);
-//             }
-//             return choiceArray;
-//           },
-//           message: "What auction would you like to place a bid in?"
-//         },
-//         {
-//           name: "bid",
-//           type: "input",
-//           message: "How much would you like to bid?"
-//         }
-//       ])
-//       .then(function(answer) {
-//         // get the information of the chosen item
-//         var chosenItem;
-//         for (var i = 0; i < results.length; i++) {
-//           if (results[i].item_name === answer.choice) {
-//             chosenItem = results[i];
-//           }
-//         }
 
-//         // determine if bid was high enough
-//         if (chosenItem.highest_bid < parseInt(answer.bid)) {
-//           // bid was high enough, so update db, let the user know, and start over
-//           connection.query(
-//             "UPDATE auctions SET ? WHERE ?",
-//             [
-//               {
-//                 highest_bid: answer.bid
-//               },
-//               {
-//                 id: chosenItem.id
-//               }
-//             ],
-//             function(error) {
-//               if (error) throw err;
-//               console.log("Bid placed successfully!");
-//               start();
-//             }
-//           );
-//         }
-//         else {
-//           // bid wasn't high enough, so apologize and start over
-//           console.log("Your bid was too low. Try again...");
-//           start();
-//         }
-//       });
-//   });
-// }
+
+
+
+
+
+
+
+    })
+  };
+
